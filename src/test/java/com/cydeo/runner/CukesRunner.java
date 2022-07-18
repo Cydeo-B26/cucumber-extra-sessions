69d9626fc2fdbb478987f75b9dd16bdeb912abbd
austinu <austin@cydeo.com>
2022-07-18 15:56:11 -0400

    avenger prep4

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features" ,
         glue = "com/cydeo/steps",
         dryRun = false,
-        tags = ""
+        tags = " "
 )
 public class CukesRunner {
 }
