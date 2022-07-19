e4351e8e7aa35fe37ed1cc697d0ea52be954d8c3
Mehmet <mhmtsytrk@gmail.com>
2022-07-19 17:22:04 -0400

    grid/jenkins

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
                 "json:target/cucumber.json"}, // cucumber report
         features = "src/test/resources/features" ,
         glue = "com/cydeo/steps",
-        dryRun = false,
+         dryRun = false,
         tags = ""
 )
 public class CukesRunner {
